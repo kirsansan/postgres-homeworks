be50118c0468d8e8d9f824d18ee0303947da83fa
kirsansan <kirill_home@mail.ru>
2023-06-10 10:04:27 +0300

    write create_tables.sql, add disconnect.sql and create_bd.sql

---
+++ homework-1/create_bd.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE north
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+( customer_id char (5) PRIMARY KEY,
+  company_name char (100),
+  contact_name char (50)
+);
+
+CREATE TABLE employees
+( employee_id int PRIMARY KEY,
+  first_name char (20),
+  last_name char (20),
+  title char (100),
+  birth_date char (10),
+  notes text
+);
+
+CREATE TABLE orders
+(	order_id int PRIMARY KEY,
+ 	customer_id char (6) UNIQUE REFERENCES customers (customer_id),
+ 	employee_id int UNIQUE REFERENCES employees (employee_id),
+ 	order_date char (10),
+ 	shio_city char (30)
+);
