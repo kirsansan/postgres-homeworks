4c3990b44a05add693a5b0f01281fb51fe416a4a
kirsansan <kirill_home@mail.ru>
2023-06-11 09:59:06 +0300

    fill simple_queries.sql in hw2

---
+++ homework-2/true_false.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+
+
+    print(False or True and False)
+    print(1 or 1 and 1)
+    print(1 or 0 and 1)
+    print(1 or 1 and 0)
+    print(1 or 0 and 0)    # order is matter, "AND" always works first
