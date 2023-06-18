1c5f0c51f96a13895c4a55933b5ef3a74ac00d7d
kirsansan <kirill_home@mail.ru>
2023-06-18 20:47:09 +0300

    fix ';' in alter_student.sql in hw4

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -17,16 +17,16 @@ CREATE TABLE student
 
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-ALTER TABLE student ADD COLUMN middle_name varchar
+ALTER TABLE student ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
-ALTER TABLE student DROP COLUMN middle_name
+ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-ALTER TABLE student RENAME birthday TO birth_date
+ALTER TABLE student RENAME birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32)
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
 INSERT INTO student (first_name, last_name, birth_date, phone ) VALUES ('Mark', 'Avrelii', '121-04-26', 'nothing');
@@ -40,4 +40,4 @@ INSERT INTO student (first_name, last_name, birth_date, phone ) VALUES ('Octavia
 
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
-TRUNCATE TABLE student RESTART IDENTITY
+TRUNCATE TABLE student RESTART IDENTITY;
