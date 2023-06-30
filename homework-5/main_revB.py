7112018552c6517a6ff1a85ca0484ff68ca6a685
kirsan <kirill_home@mail.ru>
2023-06-30 19:10:47 +0300

    realise create table suppliers

--- homework-5/main_revB.py
+++ homework-5/main_revB.py
@@ -4,22 +4,34 @@ from config.config import *
 from src.db_model import DB_Model
 from src.read_config import ConfigReader
 
+
 def main():
     # for create DATABASE will be connecting to postgres
     command_reader = ReaderSQLCommandFile()
     create_command = command_reader.get_from_sql(PATH_SQL_CREATOR)
-    params = ConfigReader(PATH_DB_INI).get_data("postgresql")    # default base
+    params = ConfigReader(PATH_DB_INI).get_data("postgresql")  # default base
     db = DB_Model('my_test_db')
 
     # create
     db.database_creator(create_command, **params)
 
     # filling from script
-    script_command = command_reader.get_from_sql(PATH_SQL_FILLER)   # read script
-    params = ConfigReader(PATH_DB_INI).get_data("my_test_db")       # set new parameters with new DATABASE name
-    db.execute_sql_script(script_command, **params)                 # execute script
+    script_command = command_reader.get_from_sql(PATH_SQL_FILLER)  # read script
+    params = ConfigReader(PATH_DB_INI).get_data("my_test_db")  # set new parameters with new DATABASE name
+    db.execute_sql_script(script_command, **params)  # execute script
 
-
+    script_command = """
+                        CREATE TABLE suppliers (
+                        supplier_id smallint NOT NULL,
+                        company_name character varying(40) NOT NULL,
+                        contact character varying(30),
+                        address character varying(60),
+                        phone character varying(24),
+                        fax character varying(24),
+                        homepage text
+                        );
+                        """
+    db.execute_sql_script(script_command, **params)
 
 
 if __name__ == '__main__':
